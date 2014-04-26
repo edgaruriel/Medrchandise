29c91b91d70a1099a3527b364b75c22b810428cf
Gabriel Rendon Nadal <gabo_rn06@hotmail.com>
2014-04-26 14:53:00 -0500

    Archivos Acerca de y Terminos y Condiciones

---
+++ terminosCondiciones.php
@@ -0,0 +1,126 @@
+<?php
+include_once("funciones/menu_header.php");
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<link href="css/header.css" rel="stylesheet" type="text/css" />
+<link href="css/footer.css" rel="stylesheet" type="text/css" />
+<link href="css/estilos_terminosCondiciones.css" rel="stylesheet" type="text/css"/>
+<link href="css/estilos_login.css" rel="stylesheet" type="text/css"/>
+<link href="css/estilos_input_text.css" rel="stylesheet" type="text/css" />
+<link href="css/estilos_botones.css" rel="stylesheet" type="text/css" />
+<link href="css/estilos_terminoscondiciones" rel="stylesheet" type="text/css">
+<title>Medrchandise</title>
+</head>
+<body>
+    <div id="cintilla">
+    </div>
+	<div id=contenedor>
+		<!--Inicio del header -->
+        <div id="div_encabezado">
+        	<div id="logotipo" ></div>
+            <div  id="btns_identificarse">
+                	<?php 
+            	 echo listarPanel();
+            	?>
+			</div>
+		<div id="div_menu" >			
+		<?php 
+		echo listarMenu();
+		?>		
+            </div>
+		</div>
+        <!--Fin del header -->
+		
+		<!--Inicio del cuerpo-->
+        <div id="cuerpo">
+        
+        <div id="titulo_TerminosCondiciones">
+        <h2>Terminos y Condiciones de Medrchandise</h2>
+        </div>
+        <div id="contenido_TerminosCondiciones">
+          <p>Lea detenidamente estos términos antes de utilizar este sitio web. La utilización del mismo se entenderá como una aceptación de estos, si usted no está de acuerdo, no utilice el sitio.<br>
+Únicamente podrá comprar productos:<br>
+1. Que estén disponibles para su venta y cuyos detalles aparezcan en Medrchandise;<br>
+2. La entrega a domicilio en toda la república Mexicana, cumpliendo con la información mínima para su entrega.<br>
+3. Medrchandise se reserva el derecho a rechazar cualquier pedido a su elección exclusiva cuando el envío no cumpla con todos los elementos de seguridad requeridos para hacer llegar el producto en las mejores condiciones<br></p>
+		<h3>Proceso de Compra</h3>
+        <p>
+        1.     Seleccione los productos que son de su interés. El sistema le permitirá ver si los productos están disponibles o no, SUJETOS A DISPONIBILIDAD.<br>
+2.     Una vez que los productos que desea adquirir han sido seleccionados, dichos productos serán incluidos en el carrito de compra. Permitiéndole ver el número de piezas y el importe de los productos seleccionados. Estos no podrán importar menos de 200 pesos, ni exceder los 50,000. En dado caso deberán hacerse varios pedidos que tendrán su costo individual por envío. Se sugiere la opción ocurre para pedidos grandes.<br>
+3.     Al final del proceso de comprobación, hará clic en el botón “confirmar pedido” para acceder a la página donde podrá confirmar el pedido  pudiendo identificar y corregir posibles errores de información.<br>
+4.     Será necesario que nos proporcione su nombre, e mail, domicilio de entrega, estado de la república en el que se encuentre, código postal (estos serán los elementos que nos permitirán establecer el cargo para envío a domicilio)  y los nombres de quienes en su ausencia pudieran recibir su pedido. Será importante para su seguridad poder enviarnos su IFE escaneado y el de la otra persona habilitada para la recepción.<br>
+5.     Al terminar deberá hacer clic en el botón “enviar pedido” este pedido virtual nos será remitido y estará sujeto a confirmación por Medrchandise vía correo electrónico donde se hará saber que hemos recibido su pedido, lo que no constituye una aceptación automática por nuestra parte de su pedido, antes de poder hacerlo.<br>
+6.     Obtendremos una autorización de la compañía comercializadora de su tarjeta de crédito o débito por el importe indicado en la página de resumen del pedido. En este momento del proceso, no sustraeremos fondos de su tarjeta, aunque el saldo disponible en su tarjeta se vera reflejado al momento de que la autorización sea aprobada. Este proceso es el procedimiento habitual que siguen los bancos verificando datos de tarjeta, códigos de seguridad. Si su tarjeta de crédito no es autorizada, el pedido no será tramitado.<br>
+7.     Una vez que ha sido aprobado Medrchandise le enviará un segundo e-mail aceptando su pedido y le será confirmado que los productos que ha solicitado le serán enviados en un lapso de 3 a 5 días hábiles. Un folio le será asignado mismo que deberá registrar y guardar para dar seguimiento al proceso<br>
+        </p>
+        <h3>Políticas de precios y validez de ofertas</h3>
+        <p>
+        Medrchandise se  reserva el derecho de poder modificar los precios, hasta que haya recibido la  confirmación del pago por parte de la institución bancaria.</p>
+        <h3>Cancelación o modificaciones de pedidos</h3>
+        <p>
+        Una vez recibido y aprobada la operación de un pedido, procedemos a la ejecución del mismo y a partir de ese momento no se aceptarán cancelaciones ni modificaciones del mismo. Medrchandise una vez que entrega el producto a la transportista deja en manos de ellos el producto y en caso de no encontrarlo, se le dejaran hasta 3 mensajes, si no fuera a recogerlo ocurre, será devuelto y si requiriese un envío nuevamente se realizaran los cargos de envío, en caso de línea fronteriza donde se requiera pagar impuestos, correrán por cuenta del comprador exclusivamente. CMM NO hace recompras de mercancía vendida.
+Los detalles de los productos disponibles para su compra (incluyendo el precio correspondiente) se encuentran especificados en Medrchandise.<br> Medrchandise adoptará todas las medidas necesarias y razonables para garantizar que todos los detalles, las descripciones y los precios de los productos que aparecen en Medrchandise son correctos cuando se incluya la información en Medrchandise.<br>
+Con los recursos a nuestro alcance se tendrá la página de la tienda Medrchandise lo más actualizada posible, la información especificada en un momento dado podría no siempre reflejar la situación exacta cuando Ud. realice un pedido.
+</p>
+		<h3>Devolución de la Mercancía</h3>
+        <p>
+        No se aceptan devoluciones, los productos que son enviados a los clientes son previamente revisados, para la entera satisfacción del cliente, por lo que se da como producto libre de defectos. Es condición indispensable que en el momento de la recepción de la mercancía se indique, en el en el formato de recepción que se firma al transportista de la compañía de mensajería, que el paquete venia íntegramente sellado.<br>
+En el supuesto caso de que el comprador regresare los productos motu propio, los gastos de transporte ocasionados por dicha devolución y en su caso los impuestos que se generen en línea fronteriza correrán por cuenta del cliente.
+</p>
+		<h3>Garantía</h3>
+        <p>
+        La garantía de todos nuestros productos será establecida por Merdchandise, a partir de un embarque con producto debidamente revisado y empacado. Casos especiales serán tratados como tales. 
+        </p>
+        <h3>Forma de Pago</h3>
+        <p>
+        Al aceptar el pedido deberá hacer su operación mediante tarjeta de crédito o debito, debiendo dar sus números, así como el número confidencial para que el sistema pueda aplicar y hacer la verificación con su banco.
+La forma de pago se verá reflejada en la factura o recibo y  en las condiciones acordadas entre el Cliente y Merdchandise y deberán ser respetadas.
+		<h3>Entrega del producto</h3>
+        <p>
+        El producto se considera entregado al salir del almacén de Merdchandise. Los plazos previstos para la entrega del material son meramente informativos, no siendo vinculantes para Merdchandise. Salvo acuerdo expreso por escrito con Merdchandise, el Cliente no tendrá derecho a solicitar la anulación de un pedido o cualquier indemnización (incluida la devolución de los portes) en el caso de que se produzca un retraso en la entrega del producto. El Cliente no podrá negarse a pagar el precio de los productos ya entregados cuando se produzca, por parte de Merdchandise, el suministro de un pedido.
+        </p>
+        <h3>Disposiciones Generales</h3>
+        <p>
+        Merdchandise se reserva el derecho a:<br>
+·       Modificar o cancelar, temporal o permanentemente, el servicio prestado a través de CMM Online Store (o cualquier parte de éste) con o sin preaviso. Merdchandise no será responsable frente a Ud. o frente a otros terceros por tal modificación o cancelación; y/o<br>
+·       Si, pese a modificarse los Términos y Condiciones y/o las Condiciones de Entrega, Ud. continúa utilizando el servicio prestado a través de Merdchandise (o cualquier parte de éste) tras dicha modificación, se considerará que ha dado su conformidad a dicho cambio. Es su responsabilidad revisar estos Términos de Uso, periódicamente, para poder estar en conocimiento de los cambios. Si no está de acuerdo con alguna modificación de los Términos y Condiciones, deberá dejar de utilizar el servicio de Merdchandise.
+        </p>
+        <br>
+        <br>
+        <br>
+        <br>
+           </div>
+        </div>
+        <!--Fin del cuerpo-->
+		
+		<!--Inicio del footer-->
+		<div id="div_footer">
+			
+			&copy; Todos los derechos reservados - Medrchandise
+		</div>
+		<div class="footer_segmento">
+			<div id="div_redes_sociales_izquierda">
+			<a href="https://www.facebook.com/"> <img class="link_redes_sociales" alt="" src="imagen/index_30.png"> </a>
+			<a href="https://twitter.com/"> <img class="link_redes_sociales" alt="" src="imagen/index_31.gif"> </a>
+			<a href="https://plus.google.com/"> <img class="link_redes_sociales" alt="" src="imagen/index_32.gif"> </a>
+			</div>			
+		</div>
+		</div>
+    <div id="div_footer">
+        <div class="leyenda">
+            &copy; Todos los derechos reservados - Medrchandise<br>
+            Desarrollado por Aldo, Javier, Jaime, Eddie, Gabriel, Edgar.
+            </div>
+		</div>
+		<div class="footer_segmento">
+			<a href="https://www.facebook.com/"> <img class="link_redes_sociales" alt="" src="imagen/index_30.png"> </a>
+			<a href="https://twitter.com/"> <img class="link_redes_sociales" alt="" src="imagen/index_31.gif"> </a>
+			<a href="https://plus.google.com/"> <img class="link_redes_sociales" alt="" src="imagen/index_32.gif"> </a>
+					
+		</div>
+		<!--Fin del footer-->
+</body>
+</html>
